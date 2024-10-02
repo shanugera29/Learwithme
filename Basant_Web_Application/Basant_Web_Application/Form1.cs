1e6e40e7f29332890bf540e31bdab9a11dc8e909
basantgera29@gmail.com <basantgera29@gmail.com>
2024-10-01 20:10:01 -0500

    added a new project

---
+++ Basant_Web_Application/Basant_Web_Application/Form1.cs
@@ -0,0 +1,15 @@
+namespace Basant_Web_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hello Johnathan ! How are you ?";
+        }
+    }
+}
